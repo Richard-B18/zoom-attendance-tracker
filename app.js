0e3218dd3f1d9e5104a9e0528e88780c167239a4
richardbryan <richard.bryan1680@gmail.com>
2023-11-07 17:07:26 +0800

    body parser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const bodyParser = require('body-parser');
 const crypto = require('crypto')
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.post('/events', (req, res) => {
     console.log(req.body);
 
