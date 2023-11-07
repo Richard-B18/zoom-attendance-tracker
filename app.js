4dda7d2f3db6e3f390af0c5cc139016ac1de0abd
richardbryan <richard.bryan1680@gmail.com>
2023-11-07 17:02:31 +0800

    setup crc based on zoom docs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const crypto = require('crypto')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -6,7 +7,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.post('/events', (req, res) => {
-    console.log(req);
+    console.log(req.body);
+
+    // Webhook request event type is a challenge-response check
+    if(request.body.event === 'endpoint.url_validation') {
+        const hashForValidate = crypto.createHmac('sha256', ZOOM_WEBHOOK_SECRET_TOKEN).update(request.body.payload.plainToken).digest('hex')
+    
+        res.status(200).json({"plainToken": request.body.payload.plainToken, "encryptedToken": hashForValidate})
+    }
+    
     res.status(200).send('received');
 })
 
